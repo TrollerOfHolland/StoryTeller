b4e365a7ac0a05c3e2e9bf113ee7c8d460dadf66
Szólát Dominik <szolatdominik@gmail.com>
2022-05-01 17:00:06 +0200

    Pages, auth, logo
    Add all the necessary pages, added the logo, added the auth pages and controllers, changed the routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/gyik', function () {
+    return view('gyik');
+});
+
+Route::get('/aff', function () {
+    return view('aff');
+});
+
+
+Route::get('/dss', function () {
+    return view('dss');
+});
+
+Route::get('/create_book', function () {
+    return view('create_book');
 });
