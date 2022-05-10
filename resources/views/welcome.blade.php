b09cfe43b60c9c364ee03472515eda0e0f031e9b
Szólát Dominik <szolatdominik@gmail.com>
2022-05-10 16:27:17 +0200

    More views and better design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,57 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="{!! asset('images/logo.png') !!}"/>
 
-        <title>StoryTeller</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{!! asset('images/logo.png') !!}" />
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <title>StoryTeller</title>
 
-        <!-- Styles -->
-        <link href="{{ URL::asset('css/welcome.css') }}" rel="stylesheet">
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <header>
-                <div class="top-left logo">
-                    <img src="images/logo.png" alt="logo">
-                </div>
-                <div class="title m-b-md">
-                    StoryTeller
-                </div>
-            </header>
-            @if (Route::has('login'))
-                <div class="center links">
-                    @auth
-                        <a href="{{ url('/home') }}">Könyveim</a>
-                        <a href="{{ url('/create_book') }}">Könyv létrehozása</a>
-                        <a href="{{ url('/logout') }}">Kijelentkezés</a>
-                    @else
-                        <a href="{{ route('login') }}">Bejelentkezés</a>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Regisztráció</a>
-                        @endif
-                    @endauth
+    <!-- Styles -->
+    <link href="{{ URL::asset('css/welcome.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        <header>
+            <div class="top-left logo">
+                <img src="images/logo.png" alt="logo">
+            </div>
+            <div class="title m-b-md">
+                StoryTeller
+            </div>
+        </header>
+        @if (Route::has('login'))
+            <div class="center links">
+                @auth
+                <div class="grid-container">
+                    <div class="grid-item"><a href="{{ url('/all_books') }}">Összes könyv</a></div>
+                    <div class="grid-item"><a href="{{ url('/my_books') }}">Könyveim</a></div>
+                    <div class="grid-item"><a href="{{ url('/create_book') }}">Könyv létrehozása</a></div>
+                    <div class="grid-item"><a href="{{ url('/all_stories') }}">Összes történet</a></div>
+                    <div class="grid-item"><a href="{{ url('/my_stories') }}">Történeteim</a></div>
+                    <div class="grid-item"><a href="{{ url('/create_story') }}">Történet létrehozása</a></div>
+                    <div class="grid-item"><a class="logout" href="{{ url('/logout') }}">Kijelentkezés</a></div>
                 </div>
-            @endif
-                <div class="extras">
-                    <a href="https://github.com/szodom/StoryTeller/blob/main/README.md">Dokumentáció</a>
-                    <a href="{{ url('/gyik') }}">GyIK</a>
-                    <a href="https://github.com/DominikSzol/StoryTeller">GitHub</a>
-                </div>
+                @else
+                    <a href="{{ route('login') }}">Bejelentkezés</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Regisztráció</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+        <div class="extras">
+            <a href="https://github.com/szodom/StoryTeller/blob/main/README.md">Dokumentáció</a>
+            <a href="{{ url('/gyik') }}">GyIK</a>
+            <a href="https://github.com/DominikSzol/StoryTeller">GitHub</a>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -26,16 +26,46 @@ Route::get('/notice', function () {
     return view('verification.notice');
 });
 
-
-
 Route::get('/gyik', function () {
     return view('gyik');
 });
 
+/*
+|--------------------------------------------------------------------------
+| Library routes
+|--------------------------------------------------------------------------
+|
+*/
 Route::get('/create_book', function () {
-    return view('create_book');
+    return view('library.create_book');
 });
 
+Route::get('/create_story', function () {
+    return view('library.create_story');
+});
+
+Route::get('/all_books', function () {
+    return view('library.all_books');
+});
+
+Route::get('/all_stories', function () {
+    return view('library.all_stories');
+});
+
+Route::get('/my_books', function () {
+    return view('library.my_books');
+});
+
+Route::get('/my_stories', function () {
+    return view('library.my_stories');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Verification routes
+|--------------------------------------------------------------------------
+|
+*/
 Route::group(['middleware' => ['auth']], function() {
     Route::get('/email/verify', '\App\Http\Controllers\Auth\VerificationController@show')->name('verification.notice');
     Route::get('/email/verify/{id}/{hash}', '\App\Http\Controllers\Auth\VerificationController@verify')->name('verification.verify')->middleware(['signed']);
